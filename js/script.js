575466bdd19db93b7b52b33584409a1d08894ae3
Rafael <rafaeluliana2017@gmail.com>
2023-04-03 17:28:16 -0300

    efeito de digitação adicionado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import filterBar from './modules/filter.js';
+import digitacaoAnim from './modules/digitacao.js';
 import showHeader from './modules/header.js';
 
 filterBar();
+digitacaoAnim();
 showHeader();
